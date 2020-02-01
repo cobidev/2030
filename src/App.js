055a3a9e13b5bb46755cb95fad510c71711c1e4a
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-02-01 17:09:42 -0300

    add game context state and set the provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import { GameProvider } from './context/gameContext';
 import MainMenu from './levels/MainMenu/MainMenu';
 import Instructions from './levels/Instructions/Instructions';
 import Credits from './levels/Credits/Credits';
@@ -14,23 +14,40 @@ import Endgame from './levels/Endgame/Endgame';
 
 import './App.css';
 
-function App() {
+const App = () => {
+  const [gameStatus, setGameStatus] = React.useState({
+    australia: false,
+    usa: false,
+    china: false,
+    ocean: false,
+    europe: false,
+  });
+
+  const handleCompleteLevel = level => {
+    setGameStatus({
+      ...gameStatus,
+      [level]: !gameStatus[level],
+    });
+  };
+
   return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={MainMenu} />
-        <Route exact path="/instructions" component={Instructions} />
-        <Route exact path="/credits" component={Credits} />
-        <Route exact path="/usa" component={UsaLevel} />
-        <Route exact path="/australia" component={AustraliaLevel} />
-        <Route exact path="/china" component={ChinaLevel} />
-        <Route exact path="/europe" component={EuropeLevel} />
-        <Route exact path="/ocean" component={OceanLevel} />
-        <Route exact path="/worldmap" component={WorldMap} />
-        <Route exact path="/endgame" component={Endgame} />
-      </Switch>
-    </div>
+    <GameProvider value={{ ...gameStatus, handleCompleteLevel }}>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={MainMenu} />
+          <Route exact path="/instructions" component={Instructions} />
+          <Route exact path="/credits" component={Credits} />
+          <Route exact path="/usa" component={UsaLevel} />
+          <Route exact path="/australia" component={AustraliaLevel} />
+          <Route exact path="/china" component={ChinaLevel} />
+          <Route exact path="/europe" component={EuropeLevel} />
+          <Route exact path="/ocean" component={OceanLevel} />
+          <Route exact path="/worldmap" component={WorldMap} />
+          <Route exact path="/endgame" component={Endgame} />
+        </Switch>
+      </div>
+    </GameProvider>
   );
-}
+};
 
 export default App;
