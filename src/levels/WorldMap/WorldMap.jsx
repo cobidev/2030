7926b31de4b9c5fce6998ee0a248537702edf59e
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-02-02 09:30:20 -0300

    Update worldmap icons behavior and update straw image size for ocean component

--- src/levels/WorldMap/WorldMap.jsx
+++ src/levels/WorldMap/WorldMap.jsx
@@ -14,26 +14,69 @@ const WorldMap = ({ history }) => {
         history.push('/end')
       ) : (
         <div className={worldMapStyles.worldMapContainer}>
-          <div className={worldMapStyles.countryButtonUsa} onClick={() => history.push('usa')}>
-            usa
-          </div>
-          <div className={worldMapStyles.countryButtonChina} onClick={() => history.push('china')}>
-            china
-          </div>
           <div
-            className={worldMapStyles.countryButtonEurope}
+            style={{ pointerEvents: context.usa === 'failed' ? 'none' : 'initial' }}
+            className={`${worldMapStyles.countryIcon} ${worldMapStyles.usa}`}
+            onClick={() => history.push('usa')}
+          >
+            {context.usa !== 'completed' ? (
+              <img
+                src={`assets/usa-${context.usa === 'failed' ? 'danger' : 'icon'}.png`}
+                alt="Wall Icon"
+              />
+            ) : null}
+          </div>
+
+          <div
+            style={{ pointerEvents: context.china === 'failed' ? 'none' : 'initial' }}
+            className={`${worldMapStyles.countryIcon} ${worldMapStyles.china}`}
+            onClick={() => history.push('china')}
+          >
+            {context.china !== 'completed' ? (
+              <img
+                src={`assets/china-${context.china === 'failed' ? 'danger' : 'icon'}.png`}
+                alt="Virus Icon"
+              />
+            ) : null}
+          </div>
+
+          <div
+            style={{ pointerEvents: context.europe === 'failed' ? 'none' : 'initial' }}
+            className={`${worldMapStyles.countryIcon} ${worldMapStyles.europe}`}
             onClick={() => history.push('europe')}
           >
-            europe
+            {context.europe !== 'completed' ? (
+              <img
+                src={`assets/europe-${context.europe === 'failed' ? 'danger' : 'icon'}.png`}
+                alt="Flag Icon"
+              />
+            ) : null}
           </div>
+
           <div
-            className={worldMapStyles.countryButtonAustralia}
+            style={{ pointerEvents: context.australia === 'failed' ? 'none' : 'initial' }}
+            className={`${worldMapStyles.countryIcon} ${worldMapStyles.australia}`}
             onClick={() => history.push('australia')}
           >
-            australia
+            {context.australia !== 'completed' ? (
+              <img
+                src={`assets/australia-${context.australia === 'failed' ? 'danger' : 'icon'}.png`}
+                alt="Fire Icon"
+              />
+            ) : null}
           </div>
-          <div className={worldMapStyles.countryButtonOcean} onClick={() => history.push('ocean')}>
-            ocean
+
+          <div
+            style={{ pointerEvents: context.ocean === 'failed' ? 'none' : 'initial' }}
+            className={`${worldMapStyles.countryIcon} ${worldMapStyles.ocean}`}
+            onClick={() => history.push('ocean')}
+          >
+            {context.ocean !== 'completed' ? (
+              <img
+                src={`assets/ocean-${context.ocean === 'failed' ? 'danger' : 'icon'}.png`}
+                alt="Trash Icon"
+              />
+            ) : null}
           </div>
         </div>
       )}
