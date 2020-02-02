0205de25feda7cc2227002745e4a597b00275cca
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-02-01 22:24:14 -0300

    update world map logic and background

--- src/levels/WorldMap/WorldMap.jsx
+++ src/levels/WorldMap/WorldMap.jsx
@@ -1,31 +1,43 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import GameContext from '../../context/gameContext';
+
 import worldMapStyles from './worldmap.module.scss';
 
 const WorldMap = ({ history }) => {
+  const context = React.useContext(GameContext);
+
   return (
-    <div className={worldMapStyles.worldMapContainer}>
-      <h1>WORLD MAP</h1>
-      <div className={worldMapStyles.countryButtonUsa} onClick={() => history.push('usa')}>
-        usa
-      </div>
-      <div className={worldMapStyles.countryButtonChina} onClick={() => history.push('china')}>
-        china
-      </div>
-      <div className={worldMapStyles.countryButtonEurope} onClick={() => history.push('europe')}>
-        europe
-      </div>
-      <div
-        className={worldMapStyles.countryButtonAustralia}
-        onClick={() => history.push('australia')}
-      >
-        australia
-      </div>
-      <div className={worldMapStyles.countryButtonOcean} onClick={() => history.push('ocean')}>
-        ocean
-      </div>
-      <div>Endgame</div>
-    </div>
+    <>
+      {!context.isGameStarted ? (
+        history.push('/')
+      ) : context.isWorldOver ? (
+        history.push('/end')
+      ) : (
+        <div className={worldMapStyles.worldMapContainer}>
+          <div className={worldMapStyles.countryButtonUsa} onClick={() => history.push('usa')}>
+            usa
+          </div>
+          <div className={worldMapStyles.countryButtonChina} onClick={() => history.push('china')}>
+            china
+          </div>
+          <div
+            className={worldMapStyles.countryButtonEurope}
+            onClick={() => history.push('europe')}
+          >
+            europe
+          </div>
+          <div
+            className={worldMapStyles.countryButtonAustralia}
+            onClick={() => history.push('australia')}
+          >
+            australia
+          </div>
+          <div className={worldMapStyles.countryButtonOcean} onClick={() => history.push('ocean')}>
+            ocean
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
