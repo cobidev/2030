a9835a2ee3bda13b92f3543cfaa4af9a8841b944
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2020-02-01 22:26:50 -0300

    update main menu logic validation

--- src/levels/MainMenu/MainMenu.jsx
+++ src/levels/MainMenu/MainMenu.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import GameContext from '../../context/gameContext';
+
+import Endgame from '../Endgame/Endgame';
 import mainMenuStyles from './mainmenu.module.scss';
 
 const MainMenu = ({ history }) => {
+  const context = React.useContext(GameContext);
+
+  const startGame = () => {
+    context.handleStartGlobalTime();
+    history.push('/worldmap');
+  };
+
   return (
-    <div>
-      <h1>Main Menu</h1>
-      <div className={mainMenuStyles.countryButton} onClick={() => history.push('worldmap')}>
-        Start Game
-      </div>
-      <div className={mainMenuStyles.countryButton} onClick={() => history.push('instructions')}>
-        Instructions
-      </div>
-      <div className={mainMenuStyles.countryButton} onClick={() => history.push('credits')}>
-        Credits
-      </div>
-    </div>
+    <>
+      {context.isWorldOver ? (
+        <Endgame />
+      ) : (
+        <div>
+          <h1>Main Menu</h1>
+          <div className={mainMenuStyles.countryButton} onClick={startGame}>
+            Start Game
+          </div>
+          <div className={mainMenuStyles.countryButton} onClick={() => history.push('credits')}>
+            Credits
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
-export default MainMenu;
+export default withRouter(MainMenu);
