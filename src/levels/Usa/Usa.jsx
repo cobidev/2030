91c093d5b1578b464e19eeb4aa6d339390322081
Lucas <lucas.tuozzo@mediamonks.com>
2020-02-02 15:19:03 -0300

    The wall was created and can be destroyed

--- src/levels/Usa/Usa.jsx
+++ src/levels/Usa/Usa.jsx
@@ -5,6 +5,31 @@ import Timer from '../../components/Timer/Timer';
 import usaStyles from './usa.module.scss';
 
 const UsaLevel = ({ history }) => {
+  const [wallLeftArray, setWallLeftArray] = React.useState([
+    '/assets/usa/Left1.png',
+    '/assets/usa/Left2.png',
+    '/assets/usa/Left3.png',
+  ]);
+  const [wallLeftUrl, setWallLeftUrl] = React.useState('');
+
+  const [wallMiddleArray, setWallMiddleArray] = React.useState([
+    '/assets/usa/Middle1.png',
+    '/assets/usa/Middle2.png',
+    '/assets/usa/Middle3.png',
+  ]);
+  const [wallMiddleUrl, setWallMiddleUrl] = React.useState('');
+
+  const [wallRightArray, setWallRightArray] = React.useState([
+    '/assets/usa/Right1.png',
+    '/assets/usa/Right2.png',
+    '/assets/usa/Right3.png',
+  ]);
+  const [wallRightUrl, setWallRightUrl] = React.useState('');
+
+  let wallLeftScore = 0;
+  let wallMiddleScore = 0;
+  let wallRightScore = 0;
+
   const context = React.useContext(GameContext);
 
   React.useEffect(() => {
@@ -13,15 +38,47 @@ const UsaLevel = ({ history }) => {
     }
   }, []);
 
-  React.useEffect(() => {
-    setTimeout(() => {
-      if (!context.usa) {
-        context.handleCompleteLevel('usa', 'failed');
-        history.push('/worldmap');
-      }
-      return;
-    }, 6000);
-  }, []);
+  // React.useEffect(() => {
+  //   setTimeout(() => {
+  //     if (!context.usa) {
+  //       context.handleCompleteLevel('usa', 'failed');
+  //       history.push('/worldmap');
+  //     }
+  //     return;
+  //   }, 6000);
+  // }, []);
+
+  const breakWall = wall => {
+    switch (wall) {
+      case 'left':
+        if (wallLeftScore < 2) {
+          wallLeftScore++;
+          setWallLeftUrl(wallLeftArray[wallLeftScore]);
+        }
+        break;
+      case 'middle':
+        if (wallMiddleScore < 2) {
+          console.log(wallMiddleScore);
+          wallMiddleScore++;
+          setWallMiddleUrl(wallMiddleArray[wallMiddleScore]);
+        }
+        break;
+      case 'right':
+        if (wallRightScore < 2) {
+          console.log('holis');
+          wallRightScore++;
+          setWallRightUrl(wallRightArray[wallRightScore]);
+        }
+        break;
+      default:
+        console.log('How did you get here?');
+    }
+    if (wallLeftScore == 2 && wallMiddleScore == 2 && wallRightScore == 2) {
+      alert('You Win');
+      context.handleCompleteLevel('ocean', 'completed');
+      history.push('/worldmap');
+    }
+  };
 
   return (
     <>
@@ -29,8 +86,25 @@ const UsaLevel = ({ history }) => {
       (context.isGameStarted && context.usa === ' failed') ? (
         history.push('/worldmap')
       ) : context.isGameStarted ? (
-        <div className={usaStyles.usa}>
-          <Timer />
+        <div className={usaStyles.usaContainer}>
+          {/* <Timer /> */}
+          <div className={usaStyles.player}></div>
+          <div className={usaStyles.trump}></div>
+          <div
+            onClick={() => breakWall('left')}
+            className={`${usaStyles.wallClickArea} ${usaStyles.wallClickLeft}`}
+          ></div>
+          <img src={wallLeftUrl || '/assets/usa/Left1.png'} className={usaStyles.wallPiece} />
+          <div
+            onClick={() => breakWall('right')}
+            className={`${usaStyles.wallClickArea} ${usaStyles.wallClickRight}`}
+          ></div>
+          <img src={wallRightUrl || '/assets/usa/Right1.png'} className={usaStyles.wallPiece} />
+          <div
+            onClick={() => breakWall('middle')}
+            className={`${usaStyles.wallClickArea} ${usaStyles.wallClickMiddle}`}
+          ></div>
+          <img src={wallMiddleUrl || '/assets/usa/Middle1.png'} className={usaStyles.wallPiece} />
         </div>
       ) : (
         history.push('/')
